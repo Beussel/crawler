cc9403d7feff35aec86d7f818e553fb0e1977d88
Beussel <al-ali_ali@web.de>
2020-11-29 18:52:35 +0100

    add DataHandler

---
+++ Handler/DataHandler.py
@@ -0,0 +1,48 @@
+import psycopg2
+
+
+class DataHandler:
+
+    def __init__(self):
+        self.connection = self.connect()
+
+    def connect(self):
+        try:
+            connection = psycopg2.connect(user="postgres",
+                                          password="postgres",
+                                          host="127.0.0.1",
+                                          port="5432",
+                                          database="WCM")
+
+            cursor = connection.cursor()
+            print(connection.get_dsn_parameters(), "\n")
+            cursor.execute("SELECT version();")
+            record = cursor.fetchone()
+            print("You are connected to - ", record, "\n")
+            return connection
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+
+    def insert(self, company):
+        sql = "INSERT INTO \"Company\" VALUES('" \
+               + company.id + "', '" + company.name + "', '" + company.branche + "', '" + company.street + "', '" \
+               + company.plz + "', '" + company.city + "', '" + company.phone_number + "', '" + company.website + "');"
+        self.execute_sql(sql)
+
+    def delete(self, id):
+        sql = "Delete from \"Company\" where id = '" + id + "';"
+        self.execute_sql(sql)
+
+    def execute_sql(self, sql):
+        try:
+            cur = self.connection.cursor()
+            cur.execute(sql)
+            self.connection.commit()
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def close_connection(self):
+        if self.connection is not None:
+            self.connection.close()
+            print("PostgreSQL connection is closed")
---
+++ Model/Company.py
@@ -0,0 +1,12 @@
+
+class Company:
+
+    def __init__(self, id, name, branche, street, plz, city, phone_number, website):
+        self.id = id
+        self.name = name
+        self.branche = branche
+        self.street = street
+        self.plz = plz
+        self.city = city
+        self.phone_number = phone_number
+        self.website = website
